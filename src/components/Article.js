54980b46c800c10ca50a93edff158d830b5e2a6f
Joseph <josephshonko99@gmail.com>
2024-04-27 14:34:04 +0300

    commiting hooks-test

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Article from './Article'
+import Header from "./Header";
 
 function Article() {
   return (
     <div>
       {/* please pass this test */}
+      <Header/>
+      <Article/>
     </div>
   );
 }
